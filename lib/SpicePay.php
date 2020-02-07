6aeed23d8ca7bc51b184b07c106ccc46d014a5d8
vladcizsol <60628002+vladcizsol@users.noreply.github.com>
2020-02-07 15:03:52 +0200

    Add files via upload

--- lib/SpicePay.php
+++ lib/SpicePay.php
@@ -4,7 +4,7 @@ namespace SpicePay;
 
 class SpicePay
 {
-    const VERSION = '3.0.4';
+    const VERSION = '1.0.0';
     const USER_AGENT_ORIGIN = 'SpicePay PHP Library';
 
     public static $siteId = '';
